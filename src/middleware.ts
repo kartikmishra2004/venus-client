0777f317d7b764aad20bfd77dcd29f378309b690
Kartik Mishra <kartik26nov2004@gmail.com>
2025-07-24 21:08:13 +0530

    Auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+    const isLoggedIn = req.cookies.get('refresh-token');
+    const { pathname } = req.nextUrl;
+    if (pathname === '/login' && isLoggedIn) {
+        return NextResponse.redirect(new URL('/', req.url));
+    }
+    if (['/', '/dashboard'].includes(pathname) && !isLoggedIn) {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+    return NextResponse.next();
+}
+export const config = { matcher: ['/', '/login', '/dashboard'], };
