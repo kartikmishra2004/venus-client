5b871210d345bbbd50b97c968244fc8f1551ea4e
Kartik Mishra <kartik26nov2004@gmail.com>
2025-07-29 23:16:15 +0530

    login responsiveness

--- src/app/login/admin/page.tsx
+++ src/app/login/admin/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AdminLogin = () => {
     return (
         <div className='w-full h-screen flex justify-center items-center'>
-            <LoginForm className='w-1/4' />
+            <LoginForm className='xl:w-[30vw] lg:w-[40vw] md:w-[70vw] sm:w-[80vw] w-full  px-4' />
         </div>
     )
 }
